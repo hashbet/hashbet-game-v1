3e1c73ca5f4485b1bfa11544e3418aa51583555a
hashbet <103619689+hashbet@users.noreply.github.com>
2022-04-14 13:32:30 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # hashbet-game-v1
 hashbet.com fair contract and result verification code
+
+# SmartContract
+Folder SmartContract is the smart algorithm contract of the website hashbet the file name is HashBet.SOL
+
+# VerifyByNodejs 
+This is a simple function for game result verification written by nodejs and etherjs
+
+## step 1
+```
+https://github.com/hashbet/hashbet-game-v1.git
+cd  VerifyByNodejs
+npm install ethers
+```
+## setp 2
+```
+Get parameters(modulo、SecretText、BlockHash) from website game records
+and update index.js params value 
+```
+
+## step 3
+
+```
+node index.js
+```
---
+++ VerifyByNodeJs/index.js
@@ -0,0 +1,24 @@
+const ethers = require('ethers');  
+
+function HashBetCheck(modulo,SecretText,BlockHash){
+    let pack = ethers.utils.solidityPack(['uint','bytes32'],[
+        SecretText, 
+        BlockHash,  
+    ]) 
+    let result = ethers.utils.keccak256(pack)  
+    let outcome =  (ethers.BigNumber.from(result)).mod(modulo) 
+    console.log("outcome = ",outcome.toString())
+}
+
+// Modulo is the number of equiprobable outcomes in a game:
+//  2 for coin flip
+//  6 for dice roll
+//  6*6 = 36 for double dice
+//  37 for roulette
+//  100 for polyroll
+let modulo = 2 // flip
+// SecretText and BlockHash get by hashbet.com game history
+let SecretText = 2569774823 
+let BlockHash = "0x4013129b0eea56f1bce5fdfd7fb4276116fce499ad706ce7b566483df053cb11"
+
+HashBetCheck(modulo,SecretText,BlockHash)
